415db56cf5a1c8c21fe87d4b0922e25466fa3832
Charumathi Devi S B <charumathi.sb@dedalus.eu>
2023-04-18 04:18:25 -0700

    task9

---
+++ Day9/Task/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FormComponent } from './dialog/form/form.component';
+import { DialogComponent } from './form/dialog/dialog.component';
+
+@NgModule({
+  declarations: [AppComponent, FormComponent, DialogComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
