d6dcc3d28d418f3d0291f09b5bacb61d8388bd7a
Charumathi Devi S B <charumathi.sb@dedalus.eu>
2023-04-25 08:11:41 -0700

    task

---
+++ 20-04-23 and 24-04-23/src/app/card/card.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  types=[
+    'All',
+    'perfume',
+    'games',
+    'clock'
+  ]
+  selected="All";
+  prodlist:any;
+  min_v=0;
+  max_v=2000;
+  isD = "none";
+  constructor(ps:ProductService){
+    ps.getProducts().subscribe(
+      {
+        next: (data:any) => this.prodlist = data,
+        error:()=>this.prodlist = []
+      }
+    )
+   
+  }
+}
